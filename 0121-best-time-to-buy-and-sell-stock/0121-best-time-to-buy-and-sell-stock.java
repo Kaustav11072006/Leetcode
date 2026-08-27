c5bd1eb000db3558b0df8f134589e691149e56c8
Kaustav Batabyal <146803781+Kaustav11072006@users.noreply.github.com>
2026-08-27 19:00:53 +0530

    Time: 1 ms (99.96%), Space: 94.6 MB (15.49%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int p = prices[0];
+        for ( int i : prices){
+           if (p>i) p=i;
+            int n =i-p;
+           max = Math.max(max,n);
+        }
+        return max;
+    }
+}
