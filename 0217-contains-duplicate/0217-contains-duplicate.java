29e8928f895b64b5672aa79fab15b52f96b021df
Kaustav Batabyal <146803781+Kaustav11072006@users.noreply.github.com>
2026-09-12 18:23:25 +0530

    Time: 39 ms (5.01%), Space: 110.8 MB (7.91%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+
+        for(int n : nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        for(int n:map.values()){
+            if(n>=2){
+                return true;
+            }
+        }
+        return false;  
+    }
+}
